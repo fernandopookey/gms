66ca935ab0a3b6a32c9ea705b4485f8375bd2014
MikoMi-ko <jatmiko.amungp@gmail.com>
2024-01-30 13:58:15 +0700

    Personal Trainer

--- resources/views/admin/layouts/content.blade.php
+++ resources/views/admin/layouts/content.blade.php
@@ -1,23 +1,3 @@
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard v1</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
@@ -26,7 +6,9 @@
                 <div class="col-lg-12 col-12">
                     <div class="card">
                         <div class="card-body">
-                            <h2>Tes</h2>
+                            @if($content)
+                                @include($content)
+                                @endif
                         </div>
                     </div>
                 </div>
@@ -37,4 +19,4 @@
         </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
-</div>
+</div> 
--- routes/web.php
+++ routes/web.php
@@ -24,15 +24,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/admin', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/personal-trainer', [PersonalTrainerController::class, 'index'])->name('personal-trainer');
+Route::get('/members', [MemberController::class, 'index']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 require __DIR__ . '/auth.php';
